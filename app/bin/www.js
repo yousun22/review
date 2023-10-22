464280b8495fc12f1f5ab2e75f3078f4f8accdbd
yousun22 <hanlddang22@gmail.com>
2023-10-23 02:51:58 +0900

    로그 관리 모듈 wiston 등록 및 설정

--- app/bin/www.js
+++ app/bin/www.js
@@ -1,9 +1,10 @@
 "use strict"
 
 const app = require("../app")
+const logger = require("../src/config/logger")
 
-const POST = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(POST, () =>{
-    console.log("Server working");
+app.listen(PORT, () =>{
+    logger.info(`${PORT} 포트에서 서버가 가동되었습니다.`);
 });
