49fd4f27fd7452c84e54e120622b811a06e81b10
yousun22 <hanlddang22@gmail.com>
2023-10-22 20:24:56 +0900

    또 다시 올리기

---
+++ app/app.js
@@ -0,0 +1,22 @@
+
+"use strict"
+
+// 모듈
+const express = require("express");
+const bodyParser =require("body-parser");
+const app = express();
+
+//라우팅
+const home = require("./src/routes/home");
+
+//앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+//app.use(express.static(`{$__dirname}/src/public`));
+app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use("/",home);//use는 미들웨어를 등록해주는 메서드
+
+module.exports =app;
---
+++ app/bin/www.js
@@ -0,0 +1,9 @@
+"use strict"
+
+const app = require("../app")
+
+const POST = 3000;
+
+app.listen(POST, () =>{
+    console.log("Server working");
+});
