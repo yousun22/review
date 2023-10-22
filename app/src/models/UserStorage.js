b2494ce40319ee232865b354f1f6c98b1d5f3ceb
yousun22 <hanlddang22@gmail.com>
2023-10-22 22:34:13 +0900

    fs모듈을 통한 회원 가입 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -5,8 +5,9 @@ const fs = require("fs").promises;
 class UserStorage{
     
     
-    static getUsers(...fields){
-        //const users = this.#users;
+    static #getUsers(data, isAll, fields){
+        const users = JSON.parse(data);
+        if(isAll) return users;
         const newUsers = fields.reduce((newUsers, field)=>{
             if (users.hasOwnProperty(field)) {
                 newUsers[field]=users[field];
@@ -15,6 +16,15 @@ class UserStorage{
         }, {});
         
         return newUsers;
+    }
+
+    static getUsers(isAll,...fields){
+        return fs
+        .readFile("./src/databases/users.json")
+        .then((data)=>{
+          return this.#getUsers(data, isAll, fields);
+         })
+        .catch(console.error);   
     }
 
     static getUserInfo(id){
@@ -38,12 +48,16 @@ class UserStorage{
             
             return userInfo;
     }
-    static save(userInfo){
-      //const users = this.#users;
-      users.id.push(userInfo.id);
-      users.name.push(userInfo.name);
-      users.psword.push(userInfo.psword);
-      return{sucess: true};
+    static async save(userInfo){
+       const users = await this.getUsers(true);
+       if(users.id.includes(userInfo.id)) {
+         throw "이미 존재하는 아이디 입니다";
+       }
+       users.id.push(userInfo.id);
+       users.name.push(userInfo.name);
+       users.psword.push(userInfo.psword);
+       fs.writeFile("./src/databases/users.json", JSON.stringify(users));
+       return {success: true};
     }
 }
 
