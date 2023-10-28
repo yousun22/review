45d88e82a5559e84e5ee08723ca60dfdf1a2663e
yousun22 <hanlddang22@gmail.com>
2023-10-28 10:55:11 +0900

    mysql

--- app/bin/www.js
+++ app/bin/www.js
@@ -2,6 +2,7 @@
 
 const app = require("../app")
 const logger = require("../src/config/logger")
+const mysql = require('mysql'); // Import the MySQL library
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,7 +15,7 @@ app.listen(PORT, () =>{
 // TODOAPP //
 
 const net = require('net');
-//const app2 = net
+//const app2 = netnpm
 //const DHT11=require("./models/DHT11");
 //const mongoose=require("mongoose");
 //require('dotenv/config');
@@ -28,6 +29,23 @@ const net = require('net');
 //const fs = require('fs');
 //const path = require('path');
 //const filePath = path.join(__dirname, 'models', 'onoff.json');
+
+const connection = mysql.createConnection({
+    host: "lecture-review1.cc2disn1eqgs.ap-northeast-2.rds.amazonaws.com",
+    user: "yousun22", // Replace with your MySQL username
+    password: "qw21as0500*",
+    database: "lecture_review1"
+});
+
+// Connect to the MySQL database
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL database: ' + err.stack);
+        return;
+    }
+    console.log('Connected to MySQL database as id ' + connection.threadId);
+});
+
 
 
 // Thing으로 부터 데이터를 받는 서버 생성
@@ -55,7 +73,19 @@ var server = net.createServer(function(socket){
 		//var obj3=JSON.parse(obj2);
         //var obj=JSON.parse(cat)
 		//cat.replace(/\n/g, "");
-        //console.log(obj2);
+
+
+
+		connection.query('INSERT INTO waterm(waterdata) VALUES (?)', [obj.data], (err, results) => {
+
+			if (err) {
+				console.error('Error inserting data into MySQL: ' + err);
+			} else {
+				console.log('Data inserted into MySQL');
+			}
+		});
+
+        console.log(obj.data);
 
 
 		// dht11.save(function(err){
@@ -76,7 +106,11 @@ var server = net.createServer(function(socket){
 	});
 });
 
-server.listen(8001, (err)=>{
+
+
+
+
+server.listen(8080, (err)=>{
     if(err){
          return console.log(err);
     }else{
@@ -84,3 +118,4 @@ server.listen(8001, (err)=>{
     }
 });
 
+///데이터 베이스에 넣어 보자
